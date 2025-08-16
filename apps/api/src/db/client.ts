fac3f81bf61984bffb0b70f1bf4df822fac471eb
Okpara Obinna <obainoforgod@gmail.com>
2025-08-16 01:39:45 +0100

    Update client.ts

--- apps/api/src/db/client.ts
+++ apps/api/src/db/client.ts
@@ -3,7 +3,12 @@ import { Pool } from 'pg';
 import * as schema from './schema';
 import { env } from '../env';
 
-const pool = new Pool({ connectionString: env.DATABASE_URL });
+const pool = new Pool({ 
+  connectionString: env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 export const db = drizzle(pool, { schema });
 export type DbClient = typeof db;
